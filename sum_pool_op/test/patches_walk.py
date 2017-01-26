36f5faea38f37f9fe87f6614ef04329668d9bbe2
elhanan7 <elhanan.ilani@gmail.com>
2017-01-26 15:54:31 +0200

    strides visualizations

--- sum_pool_op/test/patches_walk.py
+++ sum_pool_op/test/patches_walk.py
@@ -1,14 +1,20 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-
+import sys
+import os
 def main():
-    k,s,r,p = 3,255,1,'SAME'
+    d = sys.argv[1]
+    if not os.path.exists(d):
+        os.mkdir(d)
 
-    images = np.arange(256*256).reshape(1,256,256,1).astype(np.int32)
+    k,s,r,p = 11,4,1,'SAME'
+    dim = 16
+    images = np.arange(dim*dim).reshape(1,dim,dim,1).astype(np.int32)
     image = np.squeeze(images)
     sess = tf.Session()
+    fig = plt.figure()
+    plt.gray()
     with tf.Session() as sess:
         image_tf = tf.constant(images)
         patches = tf.extract_image_patches(images=images, ksizes=[1, k, k, 1], strides=[1, s, s, 1], rates=[1, r, r, 1], padding=p).eval()
@@ -16,10 +22,15 @@ def main():
         for i in range(patches.shape[0]):
             img = np.zeros_like(image)
             img.reshape(-1)[patches[i].ravel()] = 255
+
             img = img.astype(np.uint8)
             plt.clf()
-            plt.imshow(img)
-            plt.show()
-            #plt.pause(0.05)
+            plt.imshow(img, interpolation='none')
+            plt.title('d = {dim}, k = {k}, s = {s}, p = {p}'.format(**locals()))
+            plt.xticks(np.arange(0,dim)-0.5)
+            plt.yticks(np.arange(0,dim)-0.5)
+            plt.gca().grid(which='major', axis='both', linestyle='-', color='w')
+            fig.show()
+            plt.savefig('{}/frame{:0>8}.png'.format(d, i))
 if __name__ == '__main__':
     main()

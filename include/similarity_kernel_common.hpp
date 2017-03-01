04e31cbe1a2f22a5c9b3b5f73d8145b229bb2b6b
elhanan7 <elhanan.ilani@gmail.com>
2017-03-01 12:36:32 +0200

    more work, copied code from caffe similarity layer

--- include/similarity_kernel_common.hpp
+++ include/similarity_kernel_common.hpp
@@ -6,13 +6,21 @@
 #define SIMNETS_TF_SIMILARITY_LAYER_COMMON_HPP
 
 #include "tensorflow/core/framework/op_kernel.h"
-#include "tensorflow/core/framework/common_shape_fns.h>
+#include "tensorflow/core/framework/common_shape_fns.h"
+
+
+enum SimilarityFunction
+{
+    SIM_FUNC_L1,
+    SIM_FUNC_L2,
+    SIM_FUNC_CONVOLUTION
+};
 
 template <typename T>
 class SimilarityKernelCommon : public tensorflow::OpKernel
 {
 public:
-    explicit SimilarityKernelCommon(OpKernelConstruction* context);
+    explicit SimilarityKernelCommon(tensorflow::OpKernelConstruction* context);
 
 protected:
     std::vector<int> ksize_;
@@ -22,14 +30,30 @@ protected:
     T normalization_term_fudge_;
     T normalization_fudge_factor_;
     bool ignore_nan_input_;
+    SimilarityFunction similarity_function_;
 };
 
 template <typename T>
-SimilarityKernelCommon<T>::SimilarityKernelCommon(OpKernelConstruction* context)
+SimilarityKernelCommon<T>::SimilarityKernelCommon(tensorflow::OpKernelConstruction* context)
 {
     OP_REQUIRES_OK(context, context->GetAttr("padding", &padding_));
     OP_REQUIRES_OK(context, context->GetAttr("ksize", &ksize_));
     OP_REQUIRES_OK(context, context->GetAttr("strides", &stride_));
+    std::string similarity_func_str;
+    OP_REQUIRES_OK(context, context->GetAttr("similarity_function", &similarity_func_str));
+    if (similarity_func_str == "L1") {
+        similarity_function_ = SIM_FUNC_L1;
+    }
+    else if (similarity_func_str == "L2") {
+        similarity_function_ = SIM_FUNC_L2;
+    }
+    else if (similarity_func_str == "CONVOLUTION") {
+        similarity_function_ = SIM_FUNC_CONVOLUTION;
+    }
+    else {
+        assert(false, "bad similarity function");
+    }
+
     OP_REQUIRES_OK(context, context->GetAttr("normalization_term", &normalization_term_));
     OP_REQUIRES_OK(context, context->GetAttr("normalization_term_fudge", &normalization_term_fudge_));
     OP_REQUIRES_OK(context, context->GetAttr("normalization_fudge_factor_", &normalization_fudge_factor_));
--- src/similarity_kernel_cpu.cpp
+++ src/similarity_kernel_cpu.cpp
@@ -1,235 +1,171 @@
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
+#include "similarity_kernel_common.hpp"
+#include "ggemm_cpu.hpp"
 
 using namespace tensorflow;
 
-namespace
-{
-    // Divide such that DivToSmall(a, b) = c ==> b*c <= a
-    // this is  different from regular integer division for negative numbers
-    template <typename T>
-    T DivToSmaller(T a, T b)
-    {
-        auto q = a / b;
-        // assuming round-toward-zero
-        if ((a < 0) && (q * b != a)) --q;
-        return q;
-    }
-
-}
-
-template <typename T>
-class SumPoolOpCPU : public OpKernel {
- public:
-  explicit SumPoolOpCPU(OpKernelConstruction* context) : OpKernel(context)
-  {
-     OP_REQUIRES_OK(context, context->GetAttr("padding", &padding_));
-     OP_REQUIRES_OK(context, context->GetAttr("ksize", &ksize_));
-     OP_REQUIRES_OK(context, context->GetAttr("strides", &strides_));
-  }
-
-
-  void Compute(OpKernelContext* context) override {
-    // Grab the input tensor
-    const Tensor& input_tensor = context->input(0);
-    auto input = input_tensor.tensor<T,4>();
-
-    // Create an output tensor
-    Tensor* output_tensor = NULL;
-
-      int64 outRowDim, outColDim;
-      int64 outRowPad, outColPad;
-      GetWindowedOutputSize(input.dimension(1), ksize_[1], strides_[1], padding_, &outRowDim, &outRowPad);
-      GetWindowedOutputSize(input.dimension(2), ksize_[2], strides_[2], padding_, &outColDim, &outColPad);
-
-      // Somehow, the values calculated in the previous function are not compatible with what actually goes on
-      // in convolutions, nor with the documentation of the function. the following code solves the problem
-      if (padding_ == SAME)
-      {
-          outRowPad = ((outRowDim - 1) * strides_[1] + ksize_[1] - input.dimension(1)) / 2;
-          outColPad = ((outColDim - 1) * strides_[2] + ksize_[2] - input.dimension(2)) / 2;
-      }
-
-      TensorShape outputShape{input.dimension(0), outRowDim, outColDim, input.dimension(3)};
-      OP_REQUIRES_OK(context, context->allocate_output(0, outputShape,
-                                                     &output_tensor));
-      auto output = output_tensor->tensor<T, 4>();
-      SumPool(input, outRowPad, outColPad, output);
-
-      context->set_output(0, *output_tensor);
-  }
-
-    /// Compute one output element
-    /// @param batch the batch dimension
-    /// @param channel the channel we are working on
-    /// @param rowStart the starting row, could be negative when padding is on
-    /// @param colStart the starting column, could be negative when padding is enabled
-    /// @param data the input tensor with rank 4
-    /// @returns the sum of all the elements in the window
-    T ComputeElement(int64 batch, int64 channel, int64 rowStart, int64 colStart, const typename TTypes<T, 4>::ConstTensor& data)
-    {
-        T sum{0};
-        int64 lowerBoundRow = std::max(int64(0), rowStart);
-        int64 lowerBoundCol = std::max(int64(0), colStart);
-        int64 upperBoundRow = std::min(rowStart + ksize_[1], int64(data.dimension(1)));
-        int64 upperBoundCol = std::min(colStart + ksize_[2], int64(data.dimension(2)));
-
-        for (int64 row_index = lowerBoundRow; row_index < upperBoundRow; ++row_index)
-        {
-            for (int64 col_index = lowerBoundCol; col_index < upperBoundCol; ++col_index)
-            {
-                sum += data(batch, row_index, col_index, channel);
-            }
-        }
-        return sum;
-    }
-
-    /// Compute sum pool over the input tensor
-    /// @param input the input tensor with rank 4
-    /// @param padRow the row padding needed to calculate the indices
-    /// @param padCol the column padding needed to calculate the indices
-    /// @param output the output tensor, with rank 4
-    void SumPool(typename TTypes<T, 4>::ConstTensor& input, int64 padRow, int64 padCol, typename TTypes<T, 4>::Tensor& output)
-    {
-        for (int64 batch = 0; batch < output.dimension(0); ++batch)
-        {
-            for (int64 row = 0; row < output.dimension(1); ++row)
-            {
-                for (int64 col = 0; col < output.dimension(2); ++col)
-                {
-                    for (int64 channel = 0; channel < output.dimension(3); ++channel)
-                    {
-                        int64 inputRowStart = row * strides_[1] - padRow;
-                        int64 inputColStart = col * strides_[2] - padCol;
-                        output(batch, row, col, channel) = ComputeElement(batch, channel, inputRowStart, inputColStart, input);
-                    }
-                }
-            }
-        }
-    }
-
-
-private:
-   Padding padding_;
-   std::vector<int> ksize_;
-   std::vector<int> strides_;
-};
-
-template <typename T>
-class SumPoolOpGradCPU : public OpKernel {
+template<typename T>
+class SimilarityKernelCPU : public SimilarityKernelCommon<T> {
 public:
-    explicit SumPoolOpGradCPU(OpKernelConstruction* context) : OpKernel(context)
-    {
-        OP_REQUIRES_OK(context, context->GetAttr("padding", &padding_));
-        OP_REQUIRES_OK(context, context->GetAttr("ksize", &ksize_));
-        OP_REQUIRES_OK(context, context->GetAttr("strides", &strides_));
+
+    using Base = SimilarityKernelCommon<T>;
+    SimilarityKernelCPU(OpKernelConstruction *context) : Base(context) {}
+
+    typename TTypes<T, 4>::ConstTensor Im2Col(const typename TTypes<T, 4>::Tensor& input) {
+        long rows = this->ksize_[1];
+        long cols = this->ksize_[2];
+        long row_stride = this->stride_[1];
+        long col_stride = this->stride_[2];
+        auto padding = (this->padding_ == SAME)? Eigen::PADDING_SAME : Eigen::PADDING_VALID;
+        T padding_val{0};
+
+        typename TTypes<T, 5>::Tensor res = input.extract_image_patches(rows, cols, row_stride, col_stride, /*in_row_stride*/ 1, /*in_col_stride*/ 1, padding, padding_val);
+        return res;
     }
 
 
-    void Compute(OpKernelContext* context) override {
-        // Grab the input tensor
-        const Tensor& original_input_tensor = context->input(0);
-        auto original_input = original_input_tensor.tensor<T,4>();
-        const Tensor& original_output_tensor = context->input(1);
-        auto original_output = original_output_tensor.tensor<T,4>();
+    void Compute(OpKernelContext *context) override {
+        auto input = context->input(0);
+        auto templates = context->input(1);
+        auto weights = context->input(2);
 
-        // Create an output tensor
-        Tensor* output_tensor = NULL;
+        auto input_t = input.tensor<T, 4>();
+        auto templates_t = templates.tensor<T, 4>();
+        auto weights_t = weights.tensor<T, 4>();
 
-        int64 outRowDim, outColDim;
-        int64 outRowPad, outColPad;
-        GetWindowedOutputSize(original_input.dimension(1), ksize_[1], strides_[1], padding_, &outRowDim, &outRowPad);
-        GetWindowedOutputSize(original_input.dimension(2), ksize_[2], strides_[2], padding_, &outColDim, &outColPad);
+        Tensor* output = NULL;
 
-        // Somehow, the values calculated in the previous function are not compatible with what actually goes on
-        // in convolutions, nor with the documentation of the function. the following code solves the problem
-        if (padding_ == SAME)
-        {
-            outRowPad = ((outRowDim - 1) * strides_[1] + ksize_[1] - original_input.dimension(1)) / 2;
-            outColPad = ((outColDim - 1) * strides_[2] + ksize_[2] - original_input.dimension(2)) / 2;
-        }
+        int64 out_row_dim, out_col_dim;
+        int64 out_row_pad, out_col_pad;
+        GetWindowedOutputSize(input_t.dimension(1), ksize_[1], stride_[1], padding_, &out_row_dim, &out_row_pad);
+        GetWindowedOutputSize(input_t.dimension(2), ksize_[2], stride_[2], padding_, &out_col_dim, &out_col_pad);
 
-        OP_REQUIRES_OK(context, context->allocate_output(0, original_input_tensor.shape(),
-                                                         &output_tensor));
-        auto output = output_tensor->tensor<T, 4>();
-        SumPoolGrad(original_output, outRowPad, outColPad, output);
+        TensorShape output_shape{input_t.dimension(0), out_row_dim, out_col_dim, input_t.dimension(3)};
+        OP_REQUIRES_OK(context, context->allocate_output(0, output_shape,
+                                                         &output));
+        auto output_t = output->tensor<T, 4>();
 
-        context->set_output(0, *output_tensor);
-    }
+        //using Dtype = T;
+        //Dtype *col_buff = NULL;
+        //if (!is_1x1_ || normalize_patches_) {
+        //    col_buff = col_buffer_.mutable_cpu_data();
+        //}
 
-    /// Compute one output element
-    /// @param batch the batch dimension
-    /// @param channel the channel we are working on
-    /// @param rowStart the starting row, could be negative when padding is on
-    /// @param colStart the starting column, could be negative when padding is enabled
-    /// @param data the input tensor with rank 4
-    /// @returns the sum of all the elements in the window
-    T ComputeElement(int64 batch, int64 channel, int64 rowStart, int64 colStart, const typename TTypes<T, 4>::ConstTensor& data)
-    {
-        T sum{0};
-        int64 lowerBoundRow = std::max(int64(0), DivToSmaller(rowStart, int64(strides_[1])) + 1);
-        int64 lowerBoundCol = std::max(int64(0), DivToSmaller(colStart, int64(strides_[2])) + 1);
-        int64 upperBoundRow = std::min(DivToSmaller(rowStart + ksize_[1], int64(strides_[1])), int64(data.dimension(1) - 1));
-        int64 upperBoundCol = std::min(DivToSmaller(colStart + ksize_[2], int64(strides_[2])), int64(data.dimension(2) - 1));
+        // What is the interlacing stuff?
+        // What is the bottom index?
 
-        for (int64 row_index = lowerBoundRow; row_index <= upperBoundRow; ++row_index)
-        {
-            for (int64 col_index = lowerBoundCol; col_index <= upperBoundCol; ++col_index)
-            {
-                sum += data(batch, row_index, col_index, channel);
-            }
-        }
-        return sum;
-    }
+        const int params_size = num_instances_ * block_w_ * block_h_ * block_c_;
+        typename vec<Dtype>::vec2 *inter_params = static_cast<typename vec<Dtype>::vec2 *>(interlaced_params_->mutable_cpu_data());
+        interlace_cpu<Dtype>(params_size, templates, weights, inter_params);
 
-    /// Compute sum pool over the input tensor
-    /// @param original_output the original output tensor for the sum pool operation, with rank 4
-    /// @param padRow the row padding needed to calculate the indices
-    /// @param padCol the column padding needed to calculate the indices
-    /// @param output the output tensor, with rank 4
-    void SumPoolGrad(typename TTypes<T, 4>::ConstTensor& original_output, int64 padRow, int64 padCol, typename TTypes<T, 4>::Tensor& output)
-    {
-        for (int64 batch = 0; batch < output.dimension(0); ++batch)
-        {
-            for (int64 row = 0; row < output.dimension(1); ++row)
-            {
-                for (int64 col = 0; col < output.dimension(2); ++col)
-                {
-                    for (int64 channel = 0; channel < output.dimension(3); ++channel)
-                    {
-                        int64 inputRowStart = row + padRow - ksize_[1];
-                        int64 inputColStart = col + padCol - ksize_[2];
-                        output(batch, row, col, channel) = ComputeElement(batch, channel, inputRowStart, inputColStart, original_output);
-                    }
+        TTypes<T, 4>::Tensor columns_t = Im2Col(input_t);
+        T* columns_data = columns_t.data();
+
+        int batch_size = input_t.dimension(0);
+        int size_of_one_input_batch = columns_t.dimension(1) * columns_t.dimension(2) * columns_t.dimension(3) * columns_t.dimension(4);
+        int size_of_one_output_batch = output_t.dimension(1) * output_t.dimension(2) * output_t.dimension(3);
+
+        for (int batch_idx = 0; batch_idx < batch_size; ++batch_idx) {
+            T* columns_slice = &columns_t.data()[batch_idx * size_of_one_input_batch];
+
+            T* output_slice = output_t.data()[batch_idx * size_of_one_output_batch];
+
+                switch (this->layer_param_.similarity_param().similarity_function()) {
+                    case SimilarityParameter_SimilarityFunction_CONVOLUTION:
+                        ggemm_cpu
+                        < typename vec<Dtype>::vec2, Dtype, Dtype, uint8_t,
+                                sim_linear_forward < Dtype >, ggemm_add < Dtype >, false >
+                                                                                   (M_, N_, K_, inter_params, col_buff,
+                                                                                           top_data +
+                                                                                           top[bottom_idx]->offset(
+                                                                                                   n), 0, 0);
+                        break;
+                    case SimilarityParameter_SimilarityFunction_L1:
+                        ggemm_cpu
+                        < typename vec<Dtype>::vec2, Dtype, Dtype, uint8_t,
+                                sim_l1_forward < Dtype >, ggemm_add < Dtype >, false >
+                                                                               (M_, N_, K_, inter_params, col_buff,
+                                                                                       top_data +
+                                                                                       top[bottom_idx]->offset(
+                                                                                               n), 0, 0);
+                        break;
+                    case SimilarityParameter_SimilarityFunction_L2:
+                        if (normalization_term_) {
+                            if (use_log_space_weight_param_) {
+                                if (ignore_nan_input_) {
+                                    ggemm_cpu
+                                    < typename vec<Dtype>::vec2, Dtype, Dtype, Dtype,
+                                            sim_l2_normalized_forward < Dtype, true, true >, ggemm_add < Dtype >,
+                                            false >
+                                            (M_, N_, K_, inter_params, col_buff, top_data + top[bottom_idx]->offset(
+                                                    n), 0, normalization_term_fudge_);
+                                } else {
+                                    ggemm_cpu
+                                    < typename vec<Dtype>::vec2, Dtype, Dtype, Dtype,
+                                            sim_l2_normalized_forward < Dtype, true, false >, ggemm_add < Dtype >,
+                                            false >
+                                            (M_, N_, K_, inter_params, col_buff, top_data + top[bottom_idx]->offset(
+                                                    n), 0, normalization_term_fudge_);
+                                    caffe_add_scalar<Dtype>(M_ * N_, Dtype(-0.5) * Dtype(K_) * std::log(2.0 * M_PI),
+                                                            top_data + top[bottom_idx]->offset(n));
+                                }
+                            } else {
+                                if (ignore_nan_input_) {
+                                    ggemm_cpu
+                                    < typename vec<Dtype>::vec2, Dtype, Dtype, Dtype,
+                                            sim_l2_normalized_forward < Dtype, false, true >, ggemm_add < Dtype >,
+                                            false >
+                                            (M_, N_, K_, inter_params, col_buff, top_data + top[bottom_idx]->offset(
+                                                    n), 0, normalization_term_fudge_);
+                                } else {
+                                    ggemm_cpu
+                                    < typename vec<Dtype>::vec2, Dtype, Dtype, Dtype,
+                                            sim_l2_normalized_forward < Dtype, false, false >, ggemm_add < Dtype >,
+                                            false >
+                                            (M_, N_, K_, inter_params, col_buff, top_data + top[bottom_idx]->offset(
+                                                    n), 0, normalization_term_fudge_);
+                                    caffe_add_scalar<Dtype>(M_ * N_, Dtype(-0.5) * Dtype(K_) * std::log(2.0 * M_PI),
+                                                            top_data + top[bottom_idx]->offset(n));
+                                }
+                            }
+                        } else {
+                            if (use_log_space_weight_param_) {
+                                ggemm_cpu
+                                < typename vec<Dtype>::vec2, Dtype, Dtype, uint8_t,
+                                        sim_l2_forward < Dtype, true >, ggemm_add < Dtype >, false >
+                                                                                             (M_, N_, K_, inter_params, col_buff,
+                                                                                                     top_data +
+                                                                                                     top[bottom_idx]->offset(
+                                                                                                             n), 0, 0);
+                            } else {
+                                ggemm_cpu
+                                < typename vec<Dtype>::vec2, Dtype, Dtype, uint8_t,
+                                        sim_l2_forward < Dtype, false >, ggemm_add < Dtype >, false >
+                                                                                              (M_, N_, K_, inter_params, col_buff,
+                                                                                                      top_data +
+                                                                                                      top[bottom_idx]->offset(
+                                                                                                              n), 0, 0);
+                            }
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                // Add bias.
+                if (bias_term_) {
+                    caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, num_instances_,
+                                          N_, 1, (Dtype) 1., this->blobs_[2]->cpu_data(),
+                                          bias_multiplier_.cpu_data(),
+                                          (Dtype) 1., top_data + top[bottom_idx]->offset(n));
                 }
             }
         }
     }
-
-
-private:
-    Padding padding_;
-    std::vector<int> ksize_;
-    std::vector<int> strides_;
 };
 
-REGISTER_KERNEL_BUILDER(
-        Name("SumPool")
-        .Device(DEVICE_CPU)
-        .TypeConstraint<int32>("T"),
-        SumPoolOpCPU<int>);
-REGISTER_KERNEL_BUILDER(
-        Name("SumPool")
-        .Device(DEVICE_CPU)
-        .TypeConstraint<float>("T"),
-        SumPoolOpCPU<float>);
-REGISTER_KERNEL_BUILDER(
-        Name("SumPoolGrad")
-                .Device(DEVICE_CPU)
-                .TypeConstraint<int32>("T"),
-        SumPoolOpGradCPU<int>);
-REGISTER_KERNEL_BUILDER(
-        Name("SumPoolGrad")
-                .Device(DEVICE_CPU)
-                .TypeConstraint<float>("T"),
-        SumPoolOpGradCPU<float>);
+
+
+
+

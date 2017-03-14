48abbc3a6599ecf4589988af07d9eae2fc976274
elhanan7 <elhanan.ilani@gmail.com>
2017-03-14 20:26:57 +0200

    change in round_down parameter make the tests pass

--- src/similarity_kernel_gpu.cu
+++ src/similarity_kernel_gpu.cu
@@ -74,13 +74,13 @@ public:
             // im2col transformation: unroll input regions for filtering
             // into column matrix for multplication.
             if (!is_1x1_) {
-                simnets_tf::im2col_3d_gpu(
+                simnets_tf::im2col_3d_gpu<T>(
                         bottom_data + n * (height_ * width_ * channels_),
                         channels_, height_, width_,
                         block_c_, block_h_, block_w_,
                         pad_c_, pad_h_, pad_w_,
                         stride_c_, stride_h_, stride_w_,
-                        col_buff, true, out_of_bounds_value_);
+                        col_buff, false, out_of_bounds_value_);
             } else {  // special case for 1x1 convolution
                 col_buff = input_t.data() + n * (height_ * width_ * channels_);
             }
--- test/test.py
+++ test/test.py
@@ -10,19 +10,20 @@ class SimilarityTests(tf.test.TestCase):
     def testSanity(self):
         with self.test_session():
             #images = np.random.normal(size=(1,1,7,10)).astype(np.float32)
-            images = np.ones((1,1,1,4), np.float32)
+            images = np.ones((1,3,800,800), np.float32)
             images = tf.constant(images)
 
             #weights = np.absolute(np.random.normal(size=(1,1,3,3)).astype(np.float32))
-            weights = np.ones((1,1,3,3), np.float32)
+            weights = np.ones((1,3,3,3), np.float32)
             weights = tf.constant(weights)
 
             #templates = np.random.normal(size=(1,1,3,3)).astype(np.float32)
-            templates = np.zeros((1,1,3,3), np.float32)
+            templates = np.zeros((1,3,3,3), np.float32)
             templates = tf.constant(templates)
 
-            sim = similarity(images, templates, weights, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
-            sim_ref = similarity_ref(images, templates, weights, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
+            with tf.device('/cpu:0'):
+                sim = similarity(images, templates, weights, ksize=[1,3,3,1], strides=[1,2,1,1], padding='SAME', normalization_term=True)
+            sim_ref = similarity_ref(images, templates, weights, ksize=[1,3,3,1], strides=[1,2,1,1], padding='SAME', normalization_term=True)
             s = sim.eval()
             sr = sim_ref.eval()
             self.assertAllClose(s, sr, 1e-4)

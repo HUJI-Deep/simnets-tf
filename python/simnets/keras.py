1c3a1eab53626665926c9253df9e79f7a4086116
elhanan7 <elhanan.ilani@gmail.com>
2017-04-19 11:34:35 +0300

    keras layer - WIP

--- python/simnets/keras.py
+++ python/simnets/keras.py
@@ -1,22 +1,29 @@
-from keras import backend as K
-from keras.engine.topology import Layer
+from tensorflow.contrib.keras import backend as K
+from tensorflow.contrib.keras.python.keras.engine.topology import Layer
+from .ops import similarity
 import numpy as np
 
-class MyLayer(Layer):
+class Similarity(Layer):
 
     def __init__(self, output_dim, **kwargs):
+        print('init')
         self.output_dim = output_dim
-        super(MyLayer, self).__init__(**kwargs)
+        super(Similarity, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        print('build')
+        print(input_shape)
         # Create a trainable weight variable for this layer.
         self.kernel = self.add_weight(shape=(input_shape[1], self.output_dim),
                                       initializer='uniform',
                                       trainable=True)
-        super(MyLayer, self).build(input_shape)  # Be sure to call this somewhere!
+        super(Similarity, self).build(input_shape)  # Be sure to call this somewhere!
 
     def call(self, x):
-        return K.dot(x, self.kernel)
+        print('call')
+        return similarity(x, )
+        #return K.dot(x, self.kernel)
 
     def compute_output_shape(self, input_shape):
+        print('compute_output_shape')
         return (input_shape[0], self.output_dim)

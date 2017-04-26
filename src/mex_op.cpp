560f1c654c2fa996779d770851ecad2df489fd88
elhanan7 <elhanan.ilani@gmail.com>
2017-04-26 16:17:53 +0300

    added shape inference to mex layer

--- src/mex_op.cpp
+++ src/mex_op.cpp
@@ -1,89 +1,59 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
+#include "im2col.hpp"
 
 using namespace tensorflow;
 
-//Status GetMexOutputSizeFromDims(
-//        shape_inference::InferenceContext* c,
-//        shape_inference::DimensionHandle input_size,
-//        shape_inference::DimensionOrConstant filter_size, int64 stride,
-//        int padding, shape_inference::DimensionHandle* output_size) {
-//    if (stride <= 0) {
-//        return errors::InvalidArgument("Stride must be > 0, but got ", stride);
-//    }
-//
-//    // See also the parallel implementation in GetWindowedOutputSizeVerbose.
-//    TF_RETURN_IF_ERROR(c->Add(input_size, 2 * padding, output_size));
-//    TF_RETURN_IF_ERROR(c->Subtract(*output_size, filter_size, output_size));
-//    TF_RETURN_IF_ERROR(c->Divide(*output_size, stride, false, output_size));
-//    TF_RETURN_IF_ERROR(c->Add(*output_size, 1, output_size));
-//
-//    return Status::OK();
-//}
-//
-//Status MexShape(shape_inference::InferenceContext* c) {
-//    using namespace tensorflow::shape_inference;
-//    ShapeHandle input_shape;
-//    TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 4, &input_shape));
-//    ShapeHandle template_shape;
-//    TF_RETURN_IF_ERROR(c->WithRank(c->input(1), 4, &template_shape));
-//
-//    std::vector<int32> strides;
-//    TF_RETURN_IF_ERROR(c->GetAttr("strides", &strides));
-//    std::vector<int32> ksize;
-//    TF_RETURN_IF_ERROR(c->GetAttr("ksize", &ksize));
-//
-//    if (strides.size() != 4) {
-//        return errors::InvalidArgument(
-//                "Similarity requires the stride attribute to contain 4 values, but got: ",
-//                strides.size());
-//    }
-//
-//    int32 stride_rows = strides[1];
-//    int32 stride_cols = strides[2];
-//
-//    DimensionHandle batch_size_dim = c->Dim(input_shape, 0);
-//    DimensionHandle in_rows_dim = c->Dim(input_shape, 2);
-//    DimensionHandle in_cols_dim = c->Dim(input_shape, 3);
-//    DimensionHandle filter_rows_dim = c->Dim(template_shape, 2);
-//    DimensionHandle filter_cols_dim = c->Dim(template_shape, 3);
-//    DimensionHandle output_depth_dim = c->Dim(template_shape, 0);
-//
-//    DimensionHandle unused;
-//    TF_RETURN_IF_ERROR(
-//            c->Merge(c->Dim(input_shape, 1), c->Dim(template_shape, 1), &unused));
-//
-//    Padding padding;
-//    TF_RETURN_IF_ERROR(c->GetAttr("padding", &padding));
-//
-//    int pad_h, pad_w;
-//    if (padding == tensorflow::VALID)
-//    {
-//        pad_h = pad_w = 0;
-//    } else {
-//        pad_h = ksize[1] / 2;
-//        pad_w = ksize[2] / 2;
-//    }
-//
-//    DimensionHandle output_rows, output_cols;
-//    TF_RETURN_IF_ERROR(GetMexOutputSizeFromDims(
-//            c, in_rows_dim, filter_rows_dim, stride_rows, pad_h, &output_rows));
-//    TF_RETURN_IF_ERROR(GetMexOutputSizeFromDims(
-//            c, in_cols_dim, filter_cols_dim, stride_cols, pad_w, &output_cols));
-//
-//    ShapeHandle output_shape;
-//    output_shape = c->MakeShape({batch_size_dim, output_depth_dim, output_rows, output_cols});
-//
-//    c->set_output(0, output_shape);
-//    return Status::OK();
-//}
-//
-//Status MexParametersGradShape(shape_inference::InferenceContext* c) {
-//    c->set_output(0, c->input(1));
-//    c->set_output(1, c->input(2));
-//    return Status::OK();
-//}
+Status MexShape(shape_inference::InferenceContext* c) {
+    using namespace tensorflow::shape_inference;
+    ShapeHandle input_shape;
+    TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 4, &input_shape));
+
+    std::vector<int32> strides;
+    TF_RETURN_IF_ERROR(c->GetAttr("strides", &strides));
+    std::vector<int32> blocks;
+    TF_RETURN_IF_ERROR(c->GetAttr("blocks", &blocks));
+    std::vector<int32> padding;
+    TF_RETURN_IF_ERROR(c->GetAttr("padding", &padding));
+
+    if (strides.size() != 3) {
+        return errors::InvalidArgument(
+                "Mex requires the stride attribute to contain 3 values, but got: ",
+                strides.size());
+    }
+
+    int num_instances;
+    TF_RETURN_IF_ERROR(c->GetAttr("num_instances", &num_instances));
+    bool blocks_round_down;
+    TF_RETURN_IF_ERROR(c->GetAttr("blocks_round_down", &blocks_round_down));
+
+
+    int32 stride_channels = strides[0];
+    int32 stride_rows = strides[1];
+    int32 stride_cols = strides[2];
+
+    DimensionHandle batch_size_dim = c->Dim(input_shape, 0);
+    DimensionHandle in_channels_dim = c->Dim(input_shape, 1);
+    DimensionHandle in_rows_dim = c->Dim(input_shape, 2);
+    DimensionHandle in_cols_dim = c->Dim(input_shape, 3);
+
+    DimensionHandle output_channels, output_rows, output_cols;
+    TF_RETURN_IF_ERROR(simnets_tf::GetSimnetsOutputSizeFromDims(
+            c, in_channels_dim, blocks[0], stride_channels, padding[0], blocks_round_down, &output_channels));
+    TF_RETURN_IF_ERROR(c->Multiply(output_channels, num_instances, &output_channels));
+    TF_RETURN_IF_ERROR(simnets_tf::GetSimnetsOutputSizeFromDims(
+            c, in_rows_dim, blocks[1], stride_rows, padding[1], blocks_round_down, &output_rows));
+    TF_RETURN_IF_ERROR(simnets_tf::GetSimnetsOutputSizeFromDims(
+            c, in_cols_dim, blocks[2], stride_cols, padding[2], blocks_round_down, &output_cols));
+
+    ShapeHandle output_shape;
+    output_shape = c->MakeShape({batch_size_dim, output_channels, output_rows, output_cols});
+
+    c->set_output(0, output_shape);
+    return Status::OK();
+}
+
 
 REGISTER_OP("Mex")
 .Input("input: T")
@@ -100,8 +70,8 @@ REGISTER_OP("Mex")
         .Attr("blocks_round_down: bool = true")
         .Attr("use_unshared_regions: bool = true")
         .Attr("shared_offset_region: list(int) = [-1]")
-        .Attr("unshared_offset_region: list(int) = [-1]");
-        //.SetShapeFn(SimilarityShape);
+        .Attr("unshared_offset_region: list(int) = [-1]")
+        .SetShapeFn(MexShape);
 //      .Doc(R"doc(
 // Performs sum pooling on the input.
 // Each entry in `output` is the sum of the corresponding size `ksize`
